f85d9b1a7d8608ca2afdb5b08c12abc18f9d6dbb
Yunhh-Hahn <138805441+Yunhh-Hahn@users.noreply.github.com>
2024-11-18 21:09:19 -0800

    create new file structure

--- unify-front-end/app/(tabs)/_layout.tsx
+++ unify-front-end/app/(tabs)/_layout.tsx
@@ -22,7 +22,7 @@ export default function TabLayout() {
         }}
       />
       <Tabs.Screen
-        name="learn/index"
+        name="learn/modules/index"
         options={{
           title: "Learn",
         }}
