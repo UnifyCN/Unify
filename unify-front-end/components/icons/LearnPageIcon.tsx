9840f03ff846e547621767ca50f8a15f292759f1
Yunhh-Hahn <138805441+Yunhh-Hahn@users.noreply.github.com>
2025-01-01 00:58:39 -0800

    fix file path

--- unify-front-end/components/icons/LearnPageIcon.tsx
+++ unify-front-end/components/icons/LearnPageIcon.tsx
@@ -11,8 +11,8 @@ const CustomLearnIcon: React.FC<CustomIconProps> = ({ color, focused }) => (
   <Image
     source={
       focused
-        ? require('../../assets/images/LearnPageFocused.svg')
-        : require('../../assets/images/LearnPage.svg')
+        ? require('@/assets/images/LearnPageFocused.svg')
+        : require('@/assets/images/LearnPage.svg')
     }
     style={{ tintColor: color, width: 24, height: 24 } as StyleProp<ImageStyle>}
   />
