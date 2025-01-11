5bc9f894462c5c8d4119770230c3bae4e34f5744
Yunhh-Hahn <138805441+Yunhh-Hahn@users.noreply.github.com>
2025-01-11 01:34:33 -0800

    set the default header to false

--- unify-front-end/app/(tabs)/learn/_layout.tsx
+++ unify-front-end/app/(tabs)/learn/_layout.tsx
@@ -2,7 +2,8 @@ import { Stack } from 'expo-router';
 
 export default function LearnLayOut() {
     return (
-        <Stack>
+        <Stack
+            screenOptions={{ headerShown: false }}>
             <Stack.Screen name="index" options={{title: "Learn"}} />
             <Stack.Screen name="in-progress" options={{title: "In Progress"}} />
             <Stack.Screen name="Lesson-library" />
