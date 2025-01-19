d995cd4e2331f4c52f6ee7782068bd6e658241fc
Nathan Huynh <nathanhuynh171005@gmail.com>
2025-01-18 19:21:34 -0800

    something

---
+++ unify-front-end/app/(tabs)/learn/(lessons)/_layout.tsx
@@ -0,0 +1,12 @@
+import { Stack } from 'expo-router'
+
+export default function LessonLayout(){
+    return(
+        <Stack
+            screenOptions={{ headerShown: false }}>
+            <Stack.Screen />
+        </Stack>
+    )
+}
+
+
