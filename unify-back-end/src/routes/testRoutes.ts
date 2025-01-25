96cc8d52ee867ec91d8d38bb7068c80d7750eba1
PaulP1406 <ducduy0308@gmail.com>
2025-01-24 23:47:59 -0800

    removed uncessary test cases

--- unify-back-end/src/routes/testRoutes.ts
+++ unify-back-end/src/routes/testRoutes.ts
@@ -10,21 +10,6 @@ router.get('/test', (req, res) => {
   res.json({ message: 'API is working!!' });
 });
 
-router.get('/test1', async (req, res) => {
-  try {
-    const result = await pool.query('SELECT NOW()');
-    res.json(result.rows[0]);
-  } catch (err) {    
-    if (err instanceof Error) {
-      console.error(err.message);
-      res.status(500).json({ error: 'Cant query the db', details: err.message });
-    } else {
-      console.error('db error:', err);
-      res.status(500).json({ error: 'Error occurred' });
-    }
-  }
-});
-
 // Just a test from the test user table in the db
 router.get('/users', async (req, res) => {
   try {
