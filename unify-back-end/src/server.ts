06eb8c202df61541d8c9b1b2e066c9e49ed24bcb
PaulP1406 <ducduy0308@gmail.com>
2025-01-29 13:59:46 -0800

    testing validation error where Mikro cannot find the entity user

--- unify-back-end/src/server.ts
+++ unify-back-end/src/server.ts
@@ -1,13 +1,15 @@
-import { MikroORM } from '@mikro-orm/sqlite'; // change to postgres after
+import { Config, MikroORM } from '@mikro-orm/sqlite'; // change to postgres after
 import { User } from './modules/user/user.entity.js'; 
+import config from './mikro-orm.config.js';
 
 // initialize the ORM, loading the config file dynamically
-const orm = await MikroORM.init();
+const orm = await MikroORM.init(config);
 const user = new User();
 user.email = 'foo@bar.com';
 user.fullName = 'Foo Bar';
 user.password = '123456';
 
-await orm.em.persist(user).flush();
+const em = orm.em.fork();
+await em.persist(user).flush();
 
 console.log('user id is:', user.id);
