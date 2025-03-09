f99602b66f79e475c114091cd761aac656c1295b
PaulP1406 <ducduy0308@gmail.com>
2025-03-08 23:34:35 -0800

    cors adding fix err form

--- unify-back-end/src/index.ts
+++ unify-back-end/src/index.ts
@@ -1,5 +1,6 @@
 import express from 'express';
 import dotenv from 'dotenv';
+import cors from 'cors';
 import testRoutes from './routes/testRoutes';
 import authRoutes from './routes/authRoutes';
 
@@ -11,6 +12,7 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 // Middleware
+app.use(cors()); 
 app.use(express.json());
 
 // Routes
