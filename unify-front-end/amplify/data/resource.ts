c3ea64764e049c2b7efaa50c151f3f791d9aaafb
PaulP1406 <ducduy0308@gmail.com>
2025-03-31 10:04:58 -0700

    post schema

--- unify-front-end/amplify/data/resource.ts
+++ unify-front-end/amplify/data/resource.ts
@@ -4,7 +4,7 @@ const schema = a.schema({
   Todo: a
     .model({
       content: a.string(),
-    })
+  })
     .authorization((allow) => [allow.guest()]),
   
   Users: a 
@@ -13,16 +13,29 @@ const schema = a.schema({
       pronouns: a.string(),
       biography: a.string(),
       email: a.string().required(),
-      password: a.string(),
+      password: a.string().required(),
       profilePicture: a.string(), // Store in S3
-      // posts: a.hasMany(() => a.ref('Post'), 'userID'),
+      posts: a.hasMany('posts', 'postId'),
       // groups: a.hasMany(() => a.ref('GroupMember'), 'userID'),
       // followers: a.hasMany(() => a.ref('UserFollower'), 'followerID'),
       // following: a.hasMany(() => a.ref('UserFollower'), 'followingID'),
       // lessonProgress: a.hasMany(() => a.ref('LessonProgress'), 'userID'),
-    })
-    .identifier(['username']) //Primary key is user name
+  })
     .authorization((allow) => [allow.guest()]),
+  
+  Post: a.model({
+      userId: a.id(),
+      user: a.belongsTo('Users', 'userId'),
+      content: a.string(),
+      mediaUrl: a.string(), // Store in S3
+      createdAt: a.datetime(),
+      updatedAt: a.datetime(),
+      // likes: a.hasMany(() => a.ref('PostLike'), 'postID'),
+      // comments: a.hasMany(() => a.ref('PostComment'), 'postID'),
+      // tags: a.manyToMany(() => a.ref('Tag'), () => a.ref('PostTag')),
+  }),
+
+
 });
 
 export type Schema = ClientSchema<typeof schema>;
