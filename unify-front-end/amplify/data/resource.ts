25308ff85708a70b2294fec68fda3d007266d7d1
PaulP1406 <ducduy0308@gmail.com>
2025-03-31 09:27:15 -0700

    user schema

--- unify-front-end/amplify/data/resource.ts
+++ unify-front-end/amplify/data/resource.ts
@@ -6,6 +6,23 @@ const schema = a.schema({
       content: a.string(),
     })
     .authorization((allow) => [allow.guest()]),
+  
+  Users: a 
+    .model({
+      username: a.string().required(),
+      pronouns: a.string(),
+      biography: a.string(),
+      email: a.string().required(),
+      password: a.string(),
+      profilePicture: a.string(), // Store in S3
+      // posts: a.hasMany(() => a.ref('Post'), 'userID'),
+      // groups: a.hasMany(() => a.ref('GroupMember'), 'userID'),
+      // followers: a.hasMany(() => a.ref('UserFollower'), 'followerID'),
+      // following: a.hasMany(() => a.ref('UserFollower'), 'followingID'),
+      // lessonProgress: a.hasMany(() => a.ref('LessonProgress'), 'userID'),
+    })
+    .identifier(['username']) //Primary key is user name
+    .authorization((allow) => [allow.guest()]),
 });
 
 export type Schema = ClientSchema<typeof schema>;
