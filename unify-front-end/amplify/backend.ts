bffd5d41390ce2765c3db3631c0b8ef6918f9635
PaulP1406 <ducduy0308@gmail.com>
2025-04-30 19:49:20 -0700

    entry point

--- unify-front-end/amplify/backend.ts
+++ unify-front-end/amplify/backend.ts
@@ -1,8 +1,15 @@
 import { defineBackend } from '@aws-amplify/backend';
 import { auth } from './auth/resource';
 import { data } from './data/resource';
+import { generateClient } from 'aws-amplify/data';
+import type { Schema } from './data/resource';
 
 defineBackend({
   auth,
   data,
+});
+
+// Configure the API client
+export const client = generateClient<Schema>({
+  authMode: 'userPool', // Use Cognito User Pool for authentication
 });
