fc03815fade89be77466b5f2a84cd58be18ed8c9
Savar Gupta <sga152@sfu.ca>
2025-11-04 14:40:17 -0800

    removed audio permissions

---
+++ unify-front-end/ios/Pods/Headers/Public/AmplifyRTNWebBrowser/AmplifyRTNWebBrowser-Bridging-Header.h
@@ -0,0 +1,1 @@
+../../../../../node_modules/@aws-amplify/rtn-web-browser/ios/AmplifyRTNWebBrowser-Bridging-Header.h
---
+++ unify-front-end/ios/Pods/Target Support Files/AmplifyRTNWebBrowser/AmplifyRTNWebBrowser-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_AmplifyRTNWebBrowser : NSObject
+@end
+@implementation PodsDummy_AmplifyRTNWebBrowser
+@end
